744da8f6ac923a33c53b06a9e544b47368fd71ff
MarineMasta <marine.bruno98@gmail.com>
2021-06-29 21:51:17 -0500

    root

--- config/connection.js
+++ config/connection.js
@@ -11,6 +11,8 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
+    username: "root",
+    password: "root",
     port: 3306
   });
 }
